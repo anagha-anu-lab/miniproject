93822cad9c8c25251a332fe743f1a830801b5e1d
anagha-anu-lab <75839893+anagha-anu-lab@users.noreply.github.com>
2022-02-23 12:32:24 +0530

    Add files via upload

---
+++ survey/addSurveyqstn.php
@@ -0,0 +1,62 @@
+<?php $this->load->view('admin/includes/head'); ?>
+<div class="wrapper fullheight-side">
+<?php $this->load->view('admin/includes/header');
+$this->load->view('admin/includes/sidebar'); 
+$this->load->view('admin/includes/navbar'); ?>
+
+<!-- Page Content -->
+
+<div class="main-panel">
+    <div class="container">
+        <div class="page-inner">
+            <div class="page-header">
+                <h4 class="page-title"><?php echo esc($page_title) ?></h4>
+                <ul class="breadcrumbs">
+                    <li class="nav-home">
+                        <a href="">
+                            <i class="flaticon-home"></i>
+                        </a>
+                    </li>
+                    <li class="separator">
+                        <i class="flaticon-right-arrow"></i>
+                    </li>
+                    <li class="nav-home">
+                        <a href="<?php anchor_to(SURVEY_CONTROLLER . '/surveyqstn') ?>">
+                        <?php echo esc($page_title) ?>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <?php $this->load->view('admin/includes/alert'); ?>
+            <div class="row">
+                <div class="col-md-12">
+                    <form enctype="multipart/form-data"  method="POST" action="<?php anchor_to(SURVEY_CONTROLLER . '/qstnAdd') ?>">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="form-group">
+                                    <label for="survey-qstn">Survey Question <span class="text-danger">*</span></label>
+                                    <?php echo form_error('survey-qstn', '<br><span class="text-danger">', '</span>'); ?>
+                                    <input class="form-control" type="text" name="survey-qstn" placeholder="survey question" value="<?php echo set_value('survey-qstn')?>">
+                                </div>
+                                
+                            </div>
+                            <div class="card-footer">
+                                <div class="form-group text-right">
+                                    <input type="hidden" name="submit" value="Submit">
+                                    <a href="<?php anchor_to(SURVEY_CONTROLLER . '/surveyqstn'); ?>" class="btn btn-danger text-white mr-4"><i class="fas fa-arrow-left mr-1"></i> Back</a>
+                                    <button class="btn btn-success"><i class="fas fa-plus mr-1"></i> Create Question</button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- End Page Content -->
+
+</div>
+<?php $this->load->view('admin/includes/foot'); ?>
+<?php $this->load->view('admin/includes/footEnd'); ?>
